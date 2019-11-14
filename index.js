3e850b2fcd3e4122d2b7eae5562c592e7c6d2b35
Mr.T <rturgeon13@gmail.com>
2019-11-14 11:43:56 -0600

    reset

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-require('dotenv').config(); // load .env variables
-
 const server = require('./api/server.js');
 
-const port = process.env.PORT || 3300;
-server.listen(port, () => {
-  console.log(`\n=== Server listening on port ${port}\n`);
+const PORT = process.env.PORT || 3300;
+server.listen(PORT, () => {
+  console.log(`\n=== Server listening on port ${PORT} ===\n`);
 });
